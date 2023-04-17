98df547490de241070fb9ef19583d98d6fec5cff
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-17 20:53:49 +0600

    done todo list

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const inputField = document.querySelector(".todo");
-const Btns = document.querySelectorAll(".btn");
+const Btns = document.querySelectorAll('.btn');
+const checkBtns = document.querySelectorAll('#checkBtn');
+const inputAction = i => {
+  let inputField = document.querySelector(`.list-${i}`);
+  return inputField;
+};
 
-Btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+Btns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const takeInput = () => {
+      for (let i = 1; i <= 6; i++) {
+        if (
+          e.target.classList.contains(`save-button-${i}`) &&
+          !inputAction(i).value == '' &&
+          !inputAction(i).classList.contains('line-through')
+        ) {
+          inputAction(i).style.border = 'none';
+        }
+      }
+    };
     for (let i = 1; i <= 6; i++) {
       if (e.target.classList.contains(`save-button-${i}`)) {
-        console.log(2);
+        takeInput();
+      } else if (e.target.classList.contains(`delete-button-${i}`)) {
+        inputAction(i).value = '';
+        inputAction(i).style.border = '1px solid white';
+      }
+    }
+  });
+});
+checkBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    for (let i = 1; i <= 6; i++) {
+      if (e.target.classList.contains(`check-${i}`)) {
+        inputAction(i).classList.toggle('line-through');
       }
     }
   });
